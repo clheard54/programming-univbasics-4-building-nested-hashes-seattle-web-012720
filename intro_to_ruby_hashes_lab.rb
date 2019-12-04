d4a8b03147926b3fe5340b90230a3a7df7900749
Clarion Heard <clahon54@gmail.com>
2019-12-04 18:12:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -5,8 +5,8 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  base_hash
-  base_hash[:railroads][:pieces] = {}
+  monopoly = {:railroads => {} }
+  monopoly[:railroads][:pieces] = 4
 end
 
 def monopoly_with_third_tier
